e8c788f974e7ea6b7b9fcdca8e77e663edbc8488
Bruno Garcia <brunogarciarana@gmail.com>
2019-07-31 16:53:31 -0300

    First response screen

---
+++ src/main/java/br/com/casadocodigo/loja/configs/AppWebConfiguration.java
@@ -0,0 +1,22 @@
+package br.com.casadocodigo.loja.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import br.com.casadocodigo.loja.controllers.HomeController;
+
+@EnableWebMvc
+@ComponentScan(basePackageClasses = {HomeController.class})
+public class AppWebConfiguration {
+
+	@Bean
+	public InternalResourceViewResolver InternalResourceViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/view/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/view/home.jsp
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Casa do Codigo</title>
+</head>
+<body>
+	<h1>Bem vindo a Casa do Codigo</h1>
+	<table>
+		<tr>
+			<td>TDD na Prática - JAVA</td>
+			<td>Google Android</td>
+		</tr>
+		<tr>
+			<td>Certificação OCJP</td>
+			<td>Java 8 Prático</td>
+		</tr>
+	</table>
+</body>
+</html>
