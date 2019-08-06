50816c27b6ca4f417d611d017618f577690f69bd
Bruno Garcia <brunogarciarana@gmail.com>
2019-08-06 14:57:23 -0300

    JPA integrated with PERSIST method

--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutoController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutoController.java
@@ -1,12 +1,17 @@
 package br.com.casadocodigo.loja.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.casadocodigo.loja.daos.ProdutoDAO;
 import br.com.casadocodigo.loja.models.Produto;
 
 @Controller
 public class ProdutoController {
+	
+	@Autowired
+	private ProdutoDAO produtoDao;
 	
 	@RequestMapping("/produto/form")
 	public String form() {
@@ -16,6 +21,7 @@ public class ProdutoController {
 	@RequestMapping("/produto")
 	public String salva(Produto produto) {
 		System.out.println(produto);
+		produtoDao.gravar(produto);
 		return "produto/sucesso";
 	}
 }
--- src/main/java/br/com/casadocodigo/loja/models/Produto.java
+++ src/main/java/br/com/casadocodigo/loja/models/Produto.java
@@ -1,6 +1,18 @@
 package br.com.casadocodigo.loja.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.transaction.Transactional;
+
+@Entity
 public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
 	private String titulo;
 	private String descricao;
 	private int paginas;
